96d911fc73bca15f69d53285102226fc0dd74314
Ranjit67 <sahooranjit519@gmail.com>
2020-07-23 18:53:24 +0530

    My first backend commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const cors = require("cors");
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on("connected",()=>{
+    console.log("The database is connected.");
+})
+mongoose.connection.on("err",()=>{
+    console.log(err);
+})
+app.use(require("./router/route.js"))
+
+app.listen(5000,()=>{
+    console.log("The port 5000 is redy to start..");
+})
+
+
+
+// Database link:- mongodb+srv://admin-keshab:<password>@cluster0-y5crp.mongodb.net/LifesDB?retryWrites=true&w=majority
